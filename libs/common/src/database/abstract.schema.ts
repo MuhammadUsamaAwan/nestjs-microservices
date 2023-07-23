e719f014ee14fd0abdec49c3b31f382f711912dd
Muhammad Usama <underusama@gmail.com>
2023-07-23 19:14:54 +0500

    reservations crud

--- libs/common/src/database/abstract.schema.ts
+++ libs/common/src/database/abstract.schema.ts
@@ -5,8 +5,4 @@ import { SchemaTypes, Types } from 'mongoose';
 export class AbstractDocument {
   @Prop({ type: SchemaTypes.ObjectId })
   _id: Types.ObjectId;
-
-  constructor() {
-    this._id = new Types.ObjectId();
-  }
 }
