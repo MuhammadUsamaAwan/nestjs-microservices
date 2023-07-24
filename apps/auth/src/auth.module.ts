21628b1c4e2fd2495784a6fb64b6a85adc118060
MuhammadUsamaAwan <underusama@gmail.com>
2023-07-24 17:17:44 +0500

    passport local strategy

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'Joi';
+import { LocalStategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -31,6 +32,6 @@ import * as Joi from 'Joi';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStategy],
 })
 export class AuthModule {}
