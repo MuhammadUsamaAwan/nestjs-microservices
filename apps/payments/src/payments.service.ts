6f7fdbc069aa9b8a48c4e5b020ecec10d5a7d099
MuhammadUsamaAwan <underusama@gmail.com>
2023-07-25 13:20:20 +0500

    notification service

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
 import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto.ts';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy
+  ) {}
 
   private readonly stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY')!, {
     apiVersion: '2022-11-15',
@@ -14,21 +20,27 @@ export class PaymentsService {
   async createCharge({
     // card,
     amount,
+    email,
   }: PaymentsCreateChargeDto) {
     // const paymentMethod = await this.stripe.paymentMethods.create({
     //   type: 'card',
     //   card,
     // });
 
-    const paymentIntent = await this.stripe.paymentIntents.create({
-      // payment_method: paymentMethod.id,
-      amount: amount * 100,
-      confirm: true,
-      // payment_method_types: ['card'],
-      currency: 'usd',
-      payment_method: 'pm_card_visa',
+    // const paymentIntent = await this.stripe.paymentIntents.create({
+    //   // payment_method: paymentMethod.id,
+    //   amount: amount * 100,
+    //   confirm: true,
+    //   // payment_method_types: ['card'],
+    //   currency: 'usd',
+    //   payment_method: 'pm_card_visa',
+    // });
+
+    this.notificationsService.emit('notify_email', {
+      email,
+      text: `Your payment of $${amount} has completed successfully.`,
     });
 
-    return paymentIntent;
+    return;
   }
 }
